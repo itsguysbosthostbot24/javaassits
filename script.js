ba02d2c5340930b6c937de9e49adeaf472fbdab2
itsguysbosthostbot24 <77636716+itsguysbosthostbot24@users.noreply.github.com>
2024-02-23 21:49:41 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    alert("Jónapot, ez a kód lopott úgy, ahogy van (Védelem automatikusan bekapcsolva 2024.03.xx-én)");
+}, 2000);
+
+setTimeout(function() {
+    var celUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    window.location.href = celUrl;
+}, 3000);
